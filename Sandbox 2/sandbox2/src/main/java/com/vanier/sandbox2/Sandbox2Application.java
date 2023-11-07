0e87bbef3dde0c1ccd6f07a3ecfd2eff2958f9e8
Karishma <69646809+Karishma0407@users.noreply.github.com>
2023-11-07 15:58:15 -0500

    Initial Commit

---
+++ Sandbox 2/sandbox2/src/main/java/com/vanier/sandbox2/Sandbox2Application.java
@@ -0,0 +1,13 @@
+package com.vanier.sandbox2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sandbox2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sandbox2Application.class, args);
+	}
+
+}
